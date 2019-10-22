c8b19ac80381dc5fc538cdb576d850c319c516ea
unknown <ujwala.pawnarkar@gmail.com>
2019-10-22 08:17:32 -0400

    created Module7quaries file and sorted tables

--- Module7queries.sql
+++ Module7queries.sql
@@ -8,24 +8,26 @@ SELECT *
 FROM teams
 WHERE league = 'NL';
 
-SELECT CONCAT  (p.firstName, ' ', p.lastName) AS FullName, t.mascot, p.position, p.hits, p.throws, t.abbreviation
 
+
+SELECT CONCAT  (p.firstName, ' ', p.lastName) AS FullName, t.mascot, p.position, p.hits, p.throws, t.abbreviation
 FROM players p, teams t
 WHERE t.id = p.currentTeamId AND t.mascot = "Red Sox";
 
 SELECT CONCAT (p.firstName, ' ', p.lastName) AS FullName, p.position, (SELECT t.abbreviation FROM teams t WHERE t.id = p.currentTeamId) AS abbreviation, hs.homeRuns
-FROM players p,   hittingStats hs, game g
+FROM players p,   hittingStats hs, games g
 WHERE p.id = hs.playerId AND g.id = hs.gameId AND g.startTime >= '2019-06-06 00:00:00' AND startTime <='2019-06-06 23:59:59'
-ORDER BY hs.homeRuns DESC LIMIT 5; 
+ORDER BY hs.homeRuns DESC LIMIT 5;
 
 SELECT CONCAT (p.firstName, ' ', p.lastName) AS FullName, (SELECT t.abbreviation FROM teams t WHERE t.id = p.currentTeamId) AS abbreviation, ps.inningsPitched
-FROM players p, pitchingStats ps, game g 
-WHERE p.id = ps.playerId AND g.id =ps.gameId AND g.startTime >= '2019-06-06 00:00:00' AND startTime <='2019-06-06 23:59:59' AND ps.inningsPitched >= 4
+FROM players p, pitchingStats ps, games g 
+WHERE p.id = ps.playerId AND g.id = ps.gameId AND g.startTime >= '2019-06-06 00:00:00' AND startTime <='2019-06-06 23:59:59' AND ps.inningsPitched >= 4
 ORDER BY ps.earnedRuns ASC LIMIT 5; 
 
 SELECT CONCAT (p.firstName, ' ', p.lastName) AS FullName,
  p.position, (SELECT t.abbreviation FROM teams t WHERE t.id = p.currentTeamId) AS abbreviation, hs.doubles, 
-FROM players p, hittingStats hs, game g
+  (SELECT t.abbreviation FROM teams t WHERE t.id = g.homeTeamId) AS homeTeam,
+   (SELECT t.abbreviation FROM teams t WHERE t.id = g.awayTeamId) AS awayTeam
+FROM players p, hittingStats hs, games g
 WHERE p.id = hs.playerId AND g.id = hs.gameId AND g.startTime >= '2019-06-06 00:00:00' AND startTime <='2019-06-06 23:59:59'
-ORDER BY hs.doubles DESC 1;
-
+ORDER BY hs.doubles DESC LIMIT 1;
